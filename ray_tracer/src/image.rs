b344dd5f110167542c39c478077bfcac6392b5c5
Robin Gjølstad <robingjolstad@gmail.com>
2022-11-15 21:16:18 +0100

    Implement color maths

--- ray_tracer/src/image.rs
+++ ray_tracer/src/image.rs
@@ -1,4 +1,8 @@
+use std::ops::{Add, Mul, Sub};
 
+use crate::utils::is_float_equal;
+
+#[derive(Debug, Clone, Copy)]
 pub struct Color {
     red: f32,
     green: f32,
@@ -13,6 +17,63 @@ impl Color {
         }
     }
 }
+
+impl PartialEq<Color> for Color {
+    fn eq(&self, other: &Color) -> bool {
+        if is_float_equal(&self.red, other.red)
+            && is_float_equal(&self.green, other.green)
+            && is_float_equal(&self.blue, other.blue)
+        {
+            true
+        } else {
+            false
+        }
+    }
+}
+impl Add<Color> for Color {
+    type Output = Self;
+    fn add(self, rhs: Color) -> Self::Output {
+        Color {
+            red: self.red + rhs.red,
+            green: self.green + rhs.green,
+            blue: self.blue + rhs.blue,
+        }
+    }
+} 
+
+impl Sub<Color> for Color {
+    type Output = Color;
+    fn sub(self, rhs: Color) -> Self::Output {
+        Color {
+            red: self.red - rhs.red,
+            green: self.green - rhs.green,
+            blue: self.blue - rhs.blue,
+        }
+    }
+}
+
+impl Mul<Color> for Color {
+    type Output = Color;
+    fn mul(self, rhs: Color) -> Self::Output {
+        Color {
+            red: self.red * rhs.red,
+            green: self.green * rhs.green,
+            blue: self.blue * rhs.blue,
+        }
+    }
+}
+
+impl Mul<f32> for Color {
+    type Output = Color;
+    fn mul(self, rhs: f32) -> Self::Output {
+        Color {
+            red: self.red * rhs,
+            green: self.green * rhs,
+            blue: self.blue * rhs,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::utils::is_float_equal;
@@ -26,5 +87,40 @@ mod tests {
         assert!(is_float_equal(&c.red, -0.5));
         assert!(is_float_equal(&c.green, 0.4));
         assert!(is_float_equal(&c.blue, 1.7));
+    }
+
+    #[test]
+    fn adding_colors_adds_each_element() {
+        let c1 = Color::new(0.9, 0.6, 0.75);
+        let c2 = Color::new(0.7, 0.1, 0.25);
+
+        let c12 = Color::new(1.6, 0.7, 1.0);
+        assert_eq!(c1 + c2, c12);
+    }
+
+    #[test]
+    fn subtracting_colors_subtracts_each_element() {
+        let c1 = Color::new(0.9, 0.6, 0.75);
+        let c2 = Color::new(0.7, 0.1, 0.25);
+
+        let c12 = Color::new(0.2, 0.5, 0.5);
+        assert_eq!(c1 - c2, c12);
+    }
+
+    #[test]
+    fn multiplying_a_color_with_a_scalar_multiplies_each_element_with_a_scalar() {
+        let c = Color::new(0.2, 0.3, 0.4);
+
+        let c2 = Color::new(0.4, 0.6, 0.8);
+        assert_eq!(c * 2.0, c2);
+    }
+
+    #[test]
+    fn multiplying_two_colors_multiplies_their_elements() {
+        let c1 = Color::new(1.0, 0.2, 0.4);
+        let c2 = Color::new(0.9, 1.0, 0.1);
+
+        let c12 = Color::new(0.9, 0.2, 0.04);
+        assert_eq!(c1 * c2, c12);
     }
 }
--- ray_tracer/src/utils.rs
+++ ray_tracer/src/utils.rs
@@ -1,7 +1,9 @@
-use std::f32::EPSILON;
+/// A fixed value used for comparing f32
+const FLOAT_COMPARISON: f32 = 0.0000005;
 
+/// Compares two f32 and asserts whether they are "equal" or not.
 pub fn is_float_equal(actual: &f32, comparison: f32) -> bool {
-    if (actual - comparison).abs() < EPSILON {
+    if (actual - comparison).abs() < FLOAT_COMPARISON {
         true
     } else {
         false
