d2e73c880197fb3e2df8bfc611e93c3ecbb95c81
Robin Gjølstad <robingjolstad@gmail.com>
2022-12-14 21:52:31 +0100

    implement sphere normal-vectors

--- ray_tracer/src/shapes.rs
+++ ray_tracer/src/shapes.rs
@@ -1,9 +1,12 @@
-use crate::{tuples::{Point, Tuple}, matrices::Matrix};
+use crate::{
+    matrices::Matrix,
+    transformations::Transform,
+    tuples::{Point, Tuple, Vector},
+};
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 enum Objects {
-    /// Sphere(x_scale, y_scale, z_scale)
-    Sphere(f32, f32, f32),
+    Sphere(),
 }
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub struct Object {
@@ -15,9 +18,12 @@ impl Object {
     pub fn sphere() -> Self {
         Object {
             position: Tuple::new_point(0.0, 0.0, 0.0),
-            object: Objects::Sphere(1.0, 1.0, 1.0),
+            object: Objects::Sphere(),
             transform: Matrix::new_identity(),
         }
+    }
+    pub fn get_position(&self) -> Point {
+        self.position
     }
     pub fn set_transform(&mut self, trans: Matrix) {
         self.transform = trans;
@@ -25,23 +31,90 @@ impl Object {
     pub fn get_transform(&self) -> Matrix {
         self.transform
     }
-}
+    pub fn normal(&self, point: Point) -> Vector {
+        match self.object {
+            Sphere => {
+                let transform_inverse = self.get_transform().inverse().unwrap();
+                let object_point = transform_inverse * point;
+                let object_normal = object_point - Point::new_point(0.0, 0.0, 0.0);
+                let mut world_normal = transform_inverse.transpose().unwrap() * object_normal;
+                world_normal.w = 0.0;
 
-#[derive(Debug, PartialEq, Clone, Copy)]
-pub struct Sphere {
-    pub position: Point,
-    x_scale: f32,
-    y_scale: f32,
-    z_scale: f32,
-}
-
-impl Sphere {
-    pub fn new() -> Self {
-        Sphere {
-            position: Tuple::new_point(0.0, 0.0, 0.0),
-            x_scale: 1.0,
-            y_scale: 1.0,
-            z_scale: 1.0,
+                world_normal.normalize()
+            }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::f32::consts::PI;
+
+    use crate::transformations::Transform;
+
+    use super::*;
+    #[test]
+    fn the_normal_on_a_sphere_at_a_point_on_the_x_axis() {
+        let s = Object::sphere();
+        let n = s.normal(Tuple::new_point(1.0, 0.0, 0.0));
+        assert_eq!(n, Tuple::new_vector(1.0, 0.0, 0.0));
+    }
+    #[test]
+    fn the_normal_on_a_sphere_at_a_point_on_the_y_axis() {
+        let s = Object::sphere();
+        let n = s.normal(Tuple::new_point(0.0, 1.0, 0.0));
+        assert_eq!(n, Tuple::new_vector(0.0, 1.0, 0.0));
+    }
+    #[test]
+    fn the_normal_on_a_sphere_at_a_point_on_the_z_axis() {
+        let s = Object::sphere();
+        let n = s.normal(Tuple::new_point(0.0, 0.0, 1.0));
+        assert_eq!(n, Tuple::new_vector(0.0, 0.0, 1.0));
+    }
+    #[test]
+    fn the_normal_on_a_sphere_at_a_nonaxial_point() {
+        let s = Object::sphere();
+        let n = s.normal(Tuple::new_point(
+            f32::sqrt(3.0) / 3.0,
+            f32::sqrt(3.0) / 3.0,
+            f32::sqrt(3.0) / 3.0,
+        ));
+        assert_eq!(
+            n,
+            Tuple::new_vector(
+                f32::sqrt(3.0) / 3.0,
+                f32::sqrt(3.0) / 3.0,
+                f32::sqrt(3.0) / 3.0
+            )
+        );
+    }
+    #[test]
+    fn the_normal_is_a_normalized_vector() {
+        let s = Object::sphere();
+        let n = s.normal(Tuple::new_point(
+            f32::sqrt(3.0) / 3.0,
+            f32::sqrt(3.0) / 3.0,
+            f32::sqrt(3.0) / 3.0,
+        ));
+        assert_eq!(n, n.normalize())
+    }
+    #[test]
+    fn computing_the_normal_on_a_translated_sphere() {
+        let mut s = Object::sphere();
+        s.set_transform(Transform::translate(0.0, 1.0, 0.0));
+        let n = s.normal(Point::new_point(0.0, 1.70711, -0.70711));
+        assert_eq!(n, Vector::new_vector(0.0, 0.70711, -0.70711));
+    }
+    #[test]
+    fn computing_the_normal_on_a_transformed_sphere() {
+        let mut s = Object::sphere();
+        let m = Transform::scaling(1.0, 0.5, 1.0) * Transform::rotation_z(PI / 5.0);
+        s.set_transform(m);
+        let n = s.normal(Point::new_point(
+            0.0,
+            f32::sqrt(2.0) / 2.0,
+            -(f32::sqrt(2.0) / 2.0),
+        ));
+        assert_eq!(n, Vector::new_vector(0.0, 0.97014, -0.24254));
     }
 }
