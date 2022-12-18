694bfb925364a8c708256664bc592753c0df9bef
Robin Gjølstad <robingjolstad@gmail.com>
2022-12-18 22:51:38 +0100

    implement world shading

--- ray_tracer/src/world.rs
+++ ray_tracer/src/world.rs
@@ -1,4 +1,6 @@
 use crate::{
+    colors::Color,
+    intersections::{prepare_computations, IntersectComp},
     lights::Light,
     shapes::{Object, Shapes},
 };
@@ -16,6 +18,31 @@ impl World {
             lights: Vec::new(),
         }
     }
+    pub fn shade_hit(&self, comps: &IntersectComp) -> Color {
+        let mut color = Color::new(0.0, 0.0, 0.0);
+        match comps.object {
+            Object::Sphere(s) => {
+                let mat = s.get_material();
+                for lights in &self.lights {
+                    color =
+                        color + mat.lighting(&lights, &comps.point, &comps.eyev, &comps.normalv);
+                }
+            }
+        }
+        color
+    }
+
+    pub fn color_at(&self, r: &crate::rays::Ray) -> Color {
+        let int = r.intersect_world(&self);
+        let color = match int.hit() {
+            None => Color::new(0.0, 0.0, 0.0),
+            Some(int) => {
+                let comp = prepare_computations(&int, r);
+                self.shade_hit(&comp)
+            }
+        };
+        color
+    }
 }
 
 #[cfg(test)]
@@ -23,8 +50,13 @@ mod tests {
     use super::*;
 
     use crate::{
-        colors::Color, rays::Ray, shapes::sphere::Sphere, transformations::Transform,
-        tuples::Tuple, utils::is_float_equal,
+        colors::Color,
+        intersections::{prepare_computations, Intersection},
+        rays::Ray,
+        shapes::sphere::Sphere,
+        transformations::Transform,
+        tuples::Tuple,
+        utils::is_float_equal,
     };
 
     fn default_world() -> World {
@@ -91,5 +123,93 @@ mod tests {
         assert!(is_float_equal(&xs.get_element(1).unwrap().get_time(), 4.5));
         assert!(is_float_equal(&xs.get_element(2).unwrap().get_time(), 5.5));
         assert!(is_float_equal(&xs.get_element(3).unwrap().get_time(), 6.0));
+    }
+
+    #[test]
+    fn shading_an_intersection() {
+        let w = default_world();
+        let r = Ray::new(
+            Tuple::new_point(0.0, 0.0, -5.0),
+            Tuple::new_vector(0.0, 0.0, 1.0),
+        );
+        let shape = w.objects.first().unwrap();
+        let i = Intersection::new(4.0, *shape);
+        let comps = prepare_computations(&i, &r);
+        let c = w.shade_hit(&comps);
+        assert_eq!(c, Color::new(0.38066, 0.47583, 0.2855));
+    }
+    #[test]
+    fn shading_an_intersection_from_the_inside() {
+        let mut w = default_world();
+        w.lights = vec![Light::point_light(
+            &Tuple::new_point(0.0, 0.25, 0.0),
+            &Color::new(1.0, 1.0, 1.0),
+        )];
+        let r = Ray::new(
+            Tuple::new_point(0.0, 0.0, 0.0),
+            Tuple::new_vector(0.0, 0.0, 1.0),
+        );
+        let shape = w.objects[1];
+        let i = Intersection::new(0.5, shape);
+        let comps = prepare_computations(&i, &r);
+        let c = w.shade_hit(&comps);
+        assert_eq!(c, Color::new(0.90498, 0.90498, 0.90498));
+    }
+    #[test]
+    fn the_color_when_a_ray_misses() {
+        let w = default_world();
+        let r = Ray::new(
+            Tuple::new_point(0.0, 0.0, -5.0),
+            Tuple::new_vector(0.0, 1.0, 0.0),
+        );
+        let c = w.color_at(&r);
+        assert_eq!(c, Color::new(0.0, 0.0, 0.0));
+    }
+    #[test]
+    fn the_color_when_a_ray_hits() {
+        let w = default_world();
+        let r = Ray::new(
+            Tuple::new_point(0.0, 0.0, -5.0),
+            Tuple::new_vector(0.0, 0.0, 1.0),
+        );
+        let c = w.color_at(&r);
+        assert_eq!(c, Color::new(0.38066, 0.47583, 0.2855));
+    }
+    #[test]
+    fn the_color_with_an_intersection_behind_the_ray() {
+        let w = default_world();
+        let mut objects = w.objects.iter();
+        let mut outer = *objects.next().unwrap();
+        match outer {
+            Object::Sphere(s) => {
+                let mut s = s;
+                let mut mat = s.get_material();
+                mat.ambient = 1.0;
+                s.set_material(&mat);
+                outer = Object::Sphere(s).clone();
+            }
+        }
+        let mut inner = *objects.next().unwrap();
+        let mut inner_sphere = Sphere::new();
+        match inner {
+            Object::Sphere(s) => {
+                let mut s = s;
+                let mut mat = s.get_material();
+                mat.ambient = 1.0;
+                s.set_material(&mat);
+                inner_sphere = s;
+                inner = Object::Sphere(s);
+            }
+        }
+        let new_world = World {
+            lights: default_world().lights,
+            objects: vec![outer, inner],
+        };
+        let r = Ray::new(
+            Tuple::new_point(0.0, 0.0, 0.75),
+            Tuple::new_vector(0.0, 0.0, -1.0),
+        );
+        let c = new_world.color_at(&r);
+        assert_eq!(c, inner_sphere.get_material().color);
     }
 }
