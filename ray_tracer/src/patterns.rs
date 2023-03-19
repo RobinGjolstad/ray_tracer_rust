37b9c76a2aa5634facf7bfa38eb1076c22f19a26
Robin Gjølstad <robin.gjolstad@nxtech.no>
2023-03-19 15:37:54 +0100

    add separate bin for pattern testing

--- ray_tracer/src/patterns.rs
+++ ray_tracer/src/patterns.rs
@@ -37,7 +37,9 @@ impl Pattern {
     }
 
     pub fn set_transform(&mut self, transformation: Matrix) {
-        self.transform = transformation;
+        let mut transform = transformation;
+        transform.calculate_inverse().unwrap();
+        self.transform = transform;
     }
 
     fn get_transform(&self) -> Matrix {
