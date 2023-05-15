8e8a342e7f4598f73837122966859075b213342e
Robin Gjølstad <robingjolstad@gmail.com>
2023-05-15 22:04:27 +0200

    Add simple transparency to ch11_pit

--- src/bin/ch11_pit.rs
+++ src/bin/ch11_pit.rs
@@ -37,8 +37,7 @@ fn main() {
     let mut material = Material::new();
     material.color = Color::new(1.0, 0.9, 0.9);
     material.specular = 0.0;
-    material.reflective = 0.9;
-    material.refractive_index = 1.5;
+    material.reflective = 0.25;
     let mut pattern = Pattern::checker(Color::new(0.75, 0.50, 0.0), Color::new(0.0, 0.0, 1.0));
     pattern.set_transform(
         Transform::rotation_y(f64::to_radians(33.0)) * Transform::scaling(0.5, 0.5, 0.5),
@@ -49,12 +48,14 @@ fn main() {
     let mut middle = Object::new_sphere();
     middle.set_transform(&Transform::translate(-0.5, 1.0, 0.5));
     material = Material::new();
-    material.color = Color::new(0.75, 0.75, 0.75);
-    material.diffuse = 0.7;
-    material.specular = 0.3;
+    material.color = Color::new(0.05, 0.05, 0.05);
+    material.diffuse = 0.05;
+    material.ambient = 0.05;
+    material.specular = 1.0;
+    material.shininess = 300.0;
     material.transparency = 1.0;
     material.refractive_index = 2.5;
-    material.reflective = 0.15;
+    material.reflective = 1.0;
     middle.set_material(&material);
 
     let mut right = Object::new_sphere();
@@ -74,21 +75,31 @@ fn main() {
     material.color = Color::new(1.0, 0.8, 0.1);
     material.diffuse = 0.7;
     material.specular = 0.3;
-    material.reflective = 1.0;
+    material.reflective = 0.6;
     left.set_material(&material);
+
+    let mut back_wall = Object::new_plane();
+    let trans = Transform::translate(0.0, 0.0, 10.0) * Transform::rotation_x(90.0_f64.to_radians());
+    back_wall.set_transform(&trans);
+    material = Material::new();
+    material.color = Color::new(0.25, 0.25, 0.25);
+    material.shininess = 0.01;
+    material.diffuse = 0.01;
+    material.specular = 0.01;
+    back_wall.set_material(&material);
 
     let mut world = World::new();
     world.lights.push(Light::point_light(
         &Tuple::new_point(-10.0, 10.0, -10.0),
         &Color::new(1.0, 1.0, 1.0),
     ));
-    world.objects = vec![floor, left, middle, right];
+    world.objects = vec![floor, left, middle, right, back_wall];
 
     let mut camera = Camera::new(args.x_axis, args.y_axis, PI / 3.0);
     camera.set_transform(Transform::view_transform(
-        &Tuple::new_point(0.0, 1.5, -5.0),
+        &Tuple::new_point(0.0, 2.5, -5.0),
         &Tuple::new_point(0.0, 1.0, 0.0),
-        &Tuple::new_vector(0.0, 1.0, 0.0),
+        &Tuple::new_vector(0.0, 2.0, 0.0),
     ));
 
     let mut elapsed = start.elapsed();
